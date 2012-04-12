9a7fe8887a8b81321e0c8bcbb26be95883f1beef
Matthew Wilkes <git@matthewwilkes.name>
2012-04-12 22:39:31 +0100

    Serialise file support

--- src/mr/tennant/git.py
+++ src/mr/tennant/git.py
@@ -1,3 +1,8 @@
 def serialise_string(string, file_type):
     with_header = "%s %d\x00%s" % (file_type, len(string), string)
     return with_header.encode("zip")
+
+def serialise_file(f):
+    f.seek(0)
+    string = f.read()
+    return serialise_string(string, 'blob')
--- src/mr/tennant/tests/test_git_format.py
+++ src/mr/tennant/tests/test_git_format.py
@@ -1,12 +1,20 @@
 import unittest2
+from StringIO import StringIO
 
 class test_object_serialisation(unittest2.TestCase):
     
     def test_object_serialisation(self):
         from mr.tennant.git import serialise_string
         
-        hello_world = 'hello world'
-        serialised = serialise_string(hello_world, file_type="blob")
+        serialised = serialise_string("hello world", file_type="blob")
         self.assertEqual(serialised, 'x\x9cK\xca\xc9OR04d\xc8H\xcd\xc9\xc9W(\xcf/\xcaI\x01\x00={\x06~')
         
-    
+    def test_file_serialisation(self):
+        from mr.tennant.git import serialise_string, serialise_file
+        
+        hello_world = StringIO('hello world')
+        serialised = serialise_file(hello_world)
+        
+        expected_serialised = serialise_string("hello world", file_type="blob")
+        self.assertEqual(expected_serialised, serialised)
+        
