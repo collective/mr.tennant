720ef72b2d5551803f0b25881546cdd10e6de267
Matthew Wilkes <git@matthewwilkes.name>
2012-04-12 23:43:37 +0100

    Set compression level to 1, as git does

--- src/mr/tennant/git.py
+++ src/mr/tennant/git.py
@@ -1,6 +1,8 @@
+import hashlib
+
 def serialise_string(string, file_type):
-    with_header = "%s %d\x00%s" % (file_type, len(string), string)
-    return with_header.encode("zip")
+    with_header = "%s %u\x00%s" % (file_type, len(string), string)
+    return zlib.compress(with_header, 1)
 
 def serialise_file(f):
     f.seek(0)
--- src/mr/tennant/tests/test_git_format.py
+++ src/mr/tennant/tests/test_git_format.py
@@ -7,7 +7,7 @@ class test_object_serialisation(unittest2.TestCase):
         from mr.tennant.git import serialise_string
         
         serialised = serialise_string("hello world", file_type="blob")
-        self.assertEqual(serialised, 'x\x9cK\xca\xc9OR04d\xc8H\xcd\xc9\xc9W(\xcf/\xcaI\x01\x00={\x06~')
+        self.assertEqual(serialised, 'x\x01K\xca\xc9OR04d\xc8H\xcd\xc9\xc9W(\xcf/\xcaI\x01\x00={\x06~')
         
     def test_file_serialisation(self):
         from mr.tennant.git import serialise_string, serialise_file
