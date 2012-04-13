847a9b21131bbcbdbff15b621e7f55b7e0c3fc48
Matthew Wilkes <git@matthewwilkes.name>
2012-04-13 05:51:14 +0100

    Try and keep files in alphabetical order for consistency

--- src/mr/tennant/git.py
+++ src/mr/tennant/git.py
@@ -48,7 +48,7 @@ def serialise_directory(directory):
         hashed = hashlib.sha1(serialised.decode("zip")).digest()
         hashes[filename] = hashed
     tree = []
-    for filename in directory.keys():
+    for filename in sorted(directory.keys()):
         tree.append("%s %s\x00%s" % (modes[filename], filename, hashes[filename]))
     tree = "".join(tree)
     with_header = serialise_string(tree, 'tree')
